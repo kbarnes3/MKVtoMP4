53967baa618e95bec787bd7b46abc8cfb080e178
Kevin Barnes <kbarnes3@gmail.com>
2013-02-18 11:55:28 -0800

    Input now supports pattern matching with glob

--- python/MKVtoMP4/path_utils.py
+++ python/MKVtoMP4/path_utils.py
@@ -1,17 +1,21 @@
+from glob import glob
 from os.path import basename, join
 
 _mkvExtension = '.mkv'
 _mp4Extension = '.mp4'
 
 
-def hello_world(input_file, output_path):
-    filename = basename(input_file)
-    # Strip the extension if it is .mkv
-    if filename.endswith(_mkvExtension):
-        filename = filename[:-len(_mkvExtension)]
+def hello_world(input_pattern, output_path):
+    files = glob(input_pattern)
 
-    filename += _mp4Extension
-    output_file = join(output_path, filename)
+    for file in files:
+        filename = basename(file)
+        # Strip the extension if it is .mkv
+        if filename.endswith(_mkvExtension):
+            filename = filename[:-len(_mkvExtension)]
 
-    print(output_file)
+        filename += _mp4Extension
+        output_file = join(output_path, filename)
+
+        print(output_file)
 
