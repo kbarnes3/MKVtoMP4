1f356706dd97e4c0adacff7e6b2a35869f2cb7f9
Kevin Barnes <kbarnes3@gmail.com>
2013-02-18 00:53:54 -0800

    Prototype for computing output file name and path

--- python/MKVtoMP4.py
+++ python/MKVtoMP4.py
@@ -2,4 +2,4 @@ from MKVtoMP4 import *
 
 if __name__ == "__main__":
     import sys
-    hello_world(sys.argv[1])
+    hello_world(sys.argv[1], sys.argv[2])
--- python/MKVtoMP4/path_utils.py
+++ python/MKVtoMP4/path_utils.py
@@ -1,2 +1,17 @@
-def hello_world(path):
-    print(path)
+from os.path import basename, join
+
+_mkvExtension = '.mkv'
+_mp4Extension = '.mp4'
+
+
+def hello_world(input_file, output_path):
+    filename = basename(input_file)
+    # Strip the extension if it is .mkv
+    if filename.endswith(_mkvExtension):
+        filename = filename[:-len(_mkvExtension)]
+
+    filename += _mp4Extension
+    output_file = join(output_path, filename)
+
+    print(output_file)
+
