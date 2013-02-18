dec07cec33ac8180faa9f22f5fa759ef8cb796d1
Kevin Barnes <kbarnes3@gmail.com>
2013-02-18 15:07:14 -0800

    Adding usage instructions

--- python/MKVtoMP4.py
+++ python/MKVtoMP4.py
@@ -2,4 +2,7 @@ from MKVtoMP4 import *
 
 if __name__ == "__main__":
     import sys
+    if len(sys.argv) < 3:
+        print('Usage: ' + sys.argv[0] + ' source_file [source_file]... destination_directory')
+        sys.exit(1)
     hello_world(sys.argv[1], sys.argv[2])
