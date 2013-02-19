e09a482eb0d2148ec63745d9606d89c07d225cbe
Kevin Barnes <kbarnes3@gmail.com>
2013-02-19 01:11:25 -0800

    Command now supports multiple input files/patterns

--- python/MKVtoMP4.py
+++ python/MKVtoMP4.py
@@ -2,40 +2,44 @@ from glob import glob
 from os.path import basename, join
 from subprocess import call
 
-_mkvExtension = '.mkv'
-_mp4Extension = '.mp4'
+MKV_EXTENSION = '.mkv'
+MP4_EXTENSION = '.mp4'
 
 
-def convert_videos(input_pattern, output_path):
-    files = glob(input_pattern)
+def convert_videos(input_patterns, destination_directory):
+    for input_pattern in input_patterns:
+        files = glob(input_pattern)
 
-    for file in files:
-        filename = basename(file)
-        # Strip the extension if it is .mkv
-        if filename.endswith(_mkvExtension):
-            filename = filename[:-len(_mkvExtension)]
+        if len(files) == 0:
+            print('Warning: No files found matching ' + input_pattern)
 
-        filename += _mp4Extension
-        output_file = join(output_path, filename)
+        for file in files:
+            filename = basename(file)
+            # Strip the extension if it is .mkv
+            if filename.endswith(MKV_EXTENSION):
+                filename = filename[:-len(MKV_EXTENSION)]
 
-        print('Converting ' + file + ' to ' + output_file)
+            filename += MP4_EXTENSION
+            output_file = join(destination_directory, filename)
 
-        call(['ffmpeg',
-              '-i',
-              file,
-              '-c:v',
-              'copy',
-              '-c:a',
-              'aac',
-              '-cutoff',
-              '15000',
-              '-b:a',
-              '192k',
-              '-ac',
-              '2',
-              '-strict',
-              '-2',
-              output_file])
+            print('Converting ' + file + ' to ' + output_file)
+
+            call(['ffmpeg',
+                  '-i',
+                  file,
+                  '-c:v',
+                  'copy',
+                  '-c:a',
+                  'aac',
+                  '-cutoff',
+                  '15000',
+                  '-b:a',
+                  '192k',
+                  '-ac',
+                  '2',
+                  '-strict',
+                  '-2',
+                  output_file])
 
 
 if __name__ == "__main__":
@@ -43,4 +47,11 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print('Usage: ' + sys.argv[0] + ' source_file [source_file]... destination_directory')
         sys.exit(1)
-    convert_videos(sys.argv[1], sys.argv[2])
+
+    # The last argument is the destination directory
+    destination_directory = sys.argv[-1]
+
+    # The first argument is the command name. Everything in between are the files to convert
+    source_files = sys.argv[1:-1]
+
+    convert_videos(source_files, destination_directory)
