f1a4ca0e2e19956ae04ea14646e7d9c1ddd2c8a6
Kevin Barnes <kbarnes3@gmail.com>
2013-02-19 01:27:04 -0800

    Adding extra setup script parameters

--- python/setup.py
+++ python/setup.py
@@ -3,4 +3,8 @@ from distutils.core import setup
 
 setup(name='mkvtomp4',
       version='1.0',
+      description='FFmpeg wrapper for converting .mkv files to .mp4',
+      author='Kevin Barnes',
+      author_email='kbarnes3@gmail.com',
+      url='http://www.kbarnes3.com',
       py_modules=['MKVtoMP4'])
