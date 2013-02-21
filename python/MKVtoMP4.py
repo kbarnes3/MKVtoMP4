d4d61d5ce8951662fb963966aef3226192aab169
Kevin Barnes <kbarnes3@gmail.com>
2013-02-21 00:21:17 -0800

    Splitting things up to make room for a mirror mode

--- python/MKVtoMP4.py
+++ python/MKVtoMP4.py
@@ -4,6 +4,8 @@ from subprocess import call
 
 MKV_EXTENSION = '.mkv'
 MP4_EXTENSION = '.mp4'
+FILES_MODE = '-files'
+MIRROR_MODE = '-mir'
 
 
 def convert_videos(input_patterns, destination_directory):
@@ -42,16 +44,40 @@ def convert_videos(input_patterns, destination_directory):
                   output_file])
 
 
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 3:
-        print('Usage: ' + sys.argv[0] + ' source_file [source_file]... destination_directory')
+def print_usage():
+    print('Usage: ' + sys.argv[0] + ' [-files] source_file [source_file]... destination_directory')
+    print('Usage: ' + sys.argv[0] + ' -mir source_directory destination_directory')
+
+
+def _files_mode(first_file):
+    # We need at least one file and one directory from sys.argv, so the length needs to be
+    # at least 2 greater than first_file
+    if len(sys.argv) < first_file + 2:
+        print_usage()
         sys.exit(1)
 
     # The last argument is the destination directory
     destination_directory = sys.argv[-1]
 
-    # The first argument is the command name. Everything in between are the files to convert
-    source_files = sys.argv[1:-1]
+    # The file names start at sys.argv[first_file].
+    source_files = sys.argv[first_file:-1]
 
     convert_videos(source_files, destination_directory)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 3:
+        print_usage()
+        sys.exit(1)
+
+    # The first argument might be the mode to use, or it might be the first source file in files mode
+    if sys.argv[1].lower() == MIRROR_MODE:
+        pass
+    elif sys.argv[1].lower() == FILES_MODE:
+        _files_mode(2)
+    elif sys.argv[1][0] == '-':
+        print('Unknown option: ' + sys.argv[1])
+        print_usage()
+        sys.exit(1)
+    else:
+        _files_mode(1)
