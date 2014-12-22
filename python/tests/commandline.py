abaccbd41e85ecaf23388046f765ed86e978c70d
Kevin Barnes <kbarnes3@gmail.com>
2014-12-22 16:03:15 -0500

    Adding test_convert_single_wildcard

--- python/tests/commandline.py
+++ python/tests/commandline.py
@@ -1,18 +1,30 @@
 import unittest
-from mock import Mock
+from mock import patch
 
 import MKVtoMP4
 
 
 class TestCommandLine(unittest.TestCase):
     def setUp(self):
-        MKVtoMP4.exit_with_error = Mock()
-        MKVtoMP4.print_usage = Mock()
+        pass
 
     def tearDown(self):
         pass
 
-    def test_no_arguments(self):
+    @patch('MKVtoMP4.exit_with_error')
+    @patch('MKVtoMP4.print_usage')
+    def test_no_arguments(self, mock_print_usage, mock_exit_with_error):
         MKVtoMP4.process_command_line(['MKVtoMP4.py'])
-        self.assertTrue(MKVtoMP4.print_usage.called)
-        self.assertTrue(MKVtoMP4.exit_with_error.called)
+        self.assertTrue(mock_print_usage.called)
+        self.assertTrue(mock_exit_with_error.called)
+
+    @patch('MKVtoMP4.convert_videos')
+    def test_convert_single_wildcard(self, mock_convert_videos):
+
+        MKVtoMP4.process_command_line([
+            'MKVtoMP4.py',
+            'C:\\Input Videos\\*.mkv',
+            'D:\\Output Videos',
+        ])
+
+        mock_convert_videos.assert_called_with(['C:\\Input Videos\\*.mkv'], 'D:\\Output Videos')
