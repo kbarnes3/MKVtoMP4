396dab544fa0d5c6b4f408c4d2c30b74f4d086d9
Kevin Barnes <kbarnes3@gmail.com>
2015-01-06 18:29:10 -0800

    Updating version to 1.4.1

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 
 setup(name='mkvtomp4',
-      version='1.4',
+      version='1.4.1',
       description='FFmpeg wrapper for converting .mkv files to .mp4',
       author='Kevin Barnes',
       author_email='kbarnes3@gmail.com',
