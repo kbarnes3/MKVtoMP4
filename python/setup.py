ee4353846396612d1cf3af0088fe9330e739f28f
Kevin Barnes <kbarnes3@gmail.com>
2016-06-20 22:52:41 -0700

    Making a proper package

--- python/setup.py
+++ python/setup.py
@@ -7,4 +7,4 @@ setup(name='mkvtomp4',
       author='Kevin Barnes',
       author_email='kbarnes3@gmail.com',
       url='http://www.kbarnes3.com',
-      py_modules=['MKVtoMP4', 'powermanagement'])
+      packages=['MKVtoMP4'])
